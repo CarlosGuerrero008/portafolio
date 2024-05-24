84aef64d2dd856a937369ca6cc39a9a767d8786e
carlos <carlos.guerreroart@gmail.com>
2024-05-23 19:15:02 -0500

    segundo

--- src/app/sections/AboutComponent.jsx
+++ src/app/sections/AboutComponent.jsx
@@ -1,40 +1,29 @@
 import React from 'react';
 import './AboutComponent.css';
 
-const AboutComponent = ({ t = { t }, piano = { piano }, libros = { libros }, yo = { yo } }) => {
+const AboutComponent = ({ t, piano, libros, yo }) => {
   return (
     <div className="about-container">
       <div className="section">
         <h2>Sobre mi</h2>
         <div className="section-content">
-          
-        <div className="section">
-        <h2></h2>
-        <div className="section-content">
-          <div className="text text-large ">
-
-            <p>
-            {t('Biografia')}
-            </p>
+          <div className="text text-large">
+            <p>{t('Biografia')}</p>
           </div>
-          <img src={yo} alt="Piano" className="profile-pic" />
+          <img src={yo} alt="Perfil" className="profile-pic" />
         </div>
-        <div className="text ">
-              <p>
-                Tengo muchos gustos y pasatiempos, pero los más importantes, además de los que más me describen,
-                son para mí leer y tocar el piano.
-              </p>
-        </div>
-      </div>
-          
+        <div className="text">
+          <p>
+            Tengo muchos gustos y pasatiempos, pero los más importantes, además de los que más me describen,
+            son para mí leer y tocar el piano.
+          </p>
         </div>
       </div>
       <div className="section">
-        <h2></h2>
         <div className="section-content">
           <img src={libros} alt="Estante de libros" className="bookshelf-pic" />
-          <div className="text ">
-          <h2>Sobre Leer</h2>
+          <div className="text">
+            <h2>Sobre Leer</h2>
             <p>
               Me gusta leer desde bien joven. Leo mucho sobre filosofía y ciencia ficción. Mis autores favoritos son
               Albert Camus y Lovecraft. Además, mi libro favorito es "El hombre rebelde".
@@ -43,10 +32,9 @@ const AboutComponent = ({ t = { t }, piano = { piano }, libros = { libros }, yo 
         </div>
       </div>
       <div className="section">
-        <h2></h2>
         <div className="section-content">
-          <div className="text ">
-          <h2>Sobre el piano</h2>
+          <div className="text">
+            <h2>Sobre el piano</h2>
             <p>
               Toco el piano, se me da bien, me han dado muchas clases durante mi vida, mi canción preferida para tocar es
               "Chopin Nocturne Op. 9 No. 2".
